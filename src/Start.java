ec72421e9d50a4a079f864435119f448d698718b
Nagibator3000 <fetakonfeta@gmail.com>
2016-02-05 15:19:32 +0300

    -add ability to convert an image from file(add method calculateBrightness, printSymbolImg)

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,59 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 public class Start {
-    public static void main(String[] args) {
-        
+    static double[][] brightness;
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage img = ImageIO.read(new File("img1.png"));
+        img.getHeight();
+        img.getWidth();
+        brightness = new double[img.getHeight()][img.getWidth()];
+
+        calculateBrightness(img);
+
+        printSymbolImg(img);
+
+
+    }
+
+    public static void printSymbolImg(BufferedImage img) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("imgTxt.txt", "UTF-8");
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                if (brightness[i][j] < 86) {
+                    System.out.print("#");
+                    writer.print("#");
+                }
+                if (85 < brightness[i][j] && brightness[i][j] < 171) {
+                    System.out.print("*");
+                    writer.print("*");
+                }
+                if (170 < brightness[i][j]){
+                    System.out.print("'");
+                    writer.print("'");
+                }
+            }
+            System.out.println();
+            writer.println();
+        }
+        writer.close();
+    }
+
+    public static void calculateBrightness(BufferedImage img) {
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                Color color = new Color(img.getRGB(j, i));
+                int red = color.getRed();
+                int blue = color.getBlue();
+                int green = color.getGreen();
+                double y = 0.3 * red + 0.59 * green + 0.11 * blue;
+                brightness[i][j] = y;
+                System.out.print(brightness[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
